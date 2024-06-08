e4f1ea6ac6224ad5bca3d6670d1be931cf0346c4
chidoriii7 <muratore.danilo@yahoo.it>
2024-06-08 14:29:16 +0200

    vite-boolfix: milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,107 @@
 <script>
-import axios from 'axios';
-import { store } from './store';
+import axios from "axios";
+import { store } from "./store";
 export default {
-  name:'Boolfix',
-  data(){
-    return{
+  name: "Boolfix",
+  data() {
+    return {
       store,
-      goSearch:''
-    }
+      goSearch: "",
+    };
   },
   methods: {
-    searchMovie(){
-      axios.get(this.store.apiFilm, {
-        params: {
-          api_key: this.store.apiKey,
-          query: this.goSearch
-        }
-      }).then((response) => {
-
-        this.store.films = response.data.results
-        
+    searchMovie() {
+      axios
+        .get(this.store.apiFilm, {
+          params: {
+            api_key: this.store.apiKey,
+            query: this.goSearch,
+          },
         })
-      console.log(this.store.films[0])
-        
+        .then((response) => {
+          this.store.films = response.data.results;
+          console.log(response);
+        });
 
+      axios
+        .get(this.store.apiTv, {
+          params: {
+            api_key: this.store.apiKey,
+            query: this.goSearch,
+          },
+        })
+        .then((response) => {
+          this.store.tvShows = response.data.results;
 
-      
+          console.log(response);
+        });
+
+      this.goSearch = "";
     },
   },
-}
-
+};
 </script>
 
 <template>
-  <input 
-  type="text"
-  placeholder="Cerca Film"
-  v-model="goSearch">
+  <input type="text" placeholder="Cerca Film" v-model="goSearch" />
   <button @click="searchMovie">Cerca</button>
-<div class="row">
-  <div v-for="film in store.films"
-  class="col-lg-3 text-center">   
-  <div class="d-flex align-items-center flex-column mb-3">
-          <ul>
-            <!-- Titolo Originale -->
-            <li> titolo originale: {{ film.original_title }}</li>
-  
-            <!-- Titolo -->
-            <li> titolo: {{ film.title }}</li>
-        
-            <!-- Lingua -->
-            <li>lingua del film: {{ film.original_language }}</li>
-  
-            <!-- Voto -->
-            <li>numero di voti: {{ film.vote_count }}</li>
-          </ul>
-        </div>
+  <div class="row">
+    <h1 class="text-center">FILM</h1>
+    <div v-for="film in store.films" class="col-lg-3 text-center">
+      <div class="d-flex align-items-center flex-column mb-3">
+        <ul>
+          <!-- Titolo Originale -->
+          <li>titolo originale: {{ film.original_title }}</li>
+
+          <!-- Titolo -->
+          <li>titolo: {{ film.title }}</li>
+
+          <!-- Lingua -->
+          <li v-if="film.original_language === 'it'">
+            lingua del film: it <img src="/public/flags/it4x3.svg" alt="" />
+          </li>
+          <li v-else-if="film.original_language === 'en'">
+            lingua del film: en <img src="/public/flags/us4x3.svg" alt="" />
+          </li>
+          <li v-else>lingua del film: {{ film.original_language }}</li>
+
+          <!-- Voto -->
+          <li>numero di voti: {{ film.vote_count }}</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    
+    <div class="row">
+    <h1 class="text-center">SERIE TV</h1>
+    <div v-for="serietv in store.tvShows" class="col-lg-3 text-center">
+      <div class="d-flex align-items-center flex-column mb-3">
+        <ul>
+          <!-- Titolo Originale -->
+          <li>titolo originale: {{ serietv.original_name }}</li>
+
+          <!-- Titolo -->
+          <li>titolo: {{ serietv.name }}</li>
+
+          <!-- Lingua -->
+          <li v-if="serietv.original_language === 'it'">
+            lingua della serie tv: it <img src="/public/flags/it4x3.svg" alt="" />
+          </li>
+          <li v-else-if="serietv.original_language === 'en'">
+            lingua della serie tv: en <img src="/public/flags/us4x3.svg" alt="" />
+          </li>
+          <li v-else>lingua della serie tv: {{ serietv.original_language }}</li>
+
+          <!-- Voto -->
+          <li>numero di voti: {{ serietv.vote_count }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
-</div>
-
-
 </template>
 
 <style>
-  .info-film{
-    display: flex;
-  }
+img {
+  width: 20px;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive ({
     films: [],
+    tvShows: [],
     apiFilm: 'https://api.themoviedb.org/3/search/movie',
     apiTv: 'https://api.themoviedb.org/3/search/tv',
     apiKey: '2cc82828f4e166e7ef0db66a8711e6b8',
